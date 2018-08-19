8d04f1ad85fcbe835888e9483b5600f701a4df66
architekespy <42393383+architekespy@users.noreply.github.com>
2018-08-19 09:57:55 -0400

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,12 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+
+var cardTwo = "king";
+console.log("User flipped " + cardOne);
+
+
+var cardThree = "queen";
+
+var cardFour = "king";
+console.log("User flipped " + cardThree);
